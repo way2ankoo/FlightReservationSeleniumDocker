df7ddf6c78b21be56f1dc5bb16297b7bd6def9cc
ankit <way2ankoo@gmail.com>
2024-08-27 22:46:15 +0530

    add remote webdriver

--- conftest.py
+++ conftest.py
@@ -9,40 +9,39 @@ from test.tests.vendorportal.model.vendor_portal_test_data import VendorPortalTe
 from test.util.json_util import JsonUtil
 
 
-# driver = None
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="Browser choice: chrome or firefox"
+    )
 
-# def pytest_addoption(parser):
-#     parser.addoption("--browser", action="store", default="chrome", help="Browser to run tests on (chrome or firefox)")
+
+def load_data(data_paths, data_model):
+    data = []
+    for path in data_paths:
+        data_ = JsonUtil.load_json_to_dataclass(path, data_model)
+        data.append(data_)
+    return data
 
 
 def load_passenger_data():
     test_data_paths = ["test\\resources\\test-data\\flightreservation\\passenger-1.json",
                        "test\\resources\\test-data\\flightreservation\\passenger-2.json",
                        "test\\resources\\test-data\\flightreservation\\passenger-3.json"]
-    passengers = []
-    for path in test_data_paths:
-        passenger = JsonUtil.load_json_to_dataclass(path, FlightReservationTestData)
-        passengers.append(passenger)
-    return passengers
+
+    return load_data(test_data_paths, FlightReservationTestData)
 
 
 def load_vendor_data():
-    test_data_paths_ = ["test\\resources\\test-data\\vendorportal\\john.json",
-                        "test\\resources\\test-data\\vendorportal\\mike.json",
-                        "test\\resources\\test-data\\vendorportal\\sam.json"]
+    test_data_paths = ["test\\resources\\test-data\\vendorportal\\john.json",
+                       "test\\resources\\test-data\\vendorportal\\mike.json",
+                       "test\\resources\\test-data\\vendorportal\\sam.json"]
 
-    vendors = []
-    for path_ in test_data_paths_:
-        vendor = JsonUtil.load_json_to_dataclass(path_, VendorPortalTestData)
-        vendors.append(vendor)
-    return vendors
+    return load_data(test_data_paths, VendorPortalTestData)
 
 
 @pytest.fixture(scope="class", params=load_passenger_data())
 def passenger_data(request):
-    # test_data = JsonUtil.load_json_to_dataclass(request.param, FlightReservationTestData)
     return request.param
-    # return test_data
 
 
 @pytest.fixture(scope="class", params=load_vendor_data())
@@ -51,20 +50,22 @@ def vendor_data(request):
 
 
 @pytest.fixture(scope="class")
+# @pytest.fixture(scope="class", params=["chrome", "firefox"])
 def driver(request):
     # global driver
     # for local execution
-    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome()
 
     # for remote execution Eg. selenium grid
     # browser = request.config.getoption("--browser")
-    # selenium_grid_url = "http://localhost:4444/wd/hub"
-    # if browser == "chrome":
-    #     options = webdriver.ChromeOptions()
-    # elif browser == "firefox":
-    #     options = webdriver.FirefoxOptions()
+    browser = request.param
+    selenium_grid_url = "http://localhost:4444/wd/hub"
+    if browser == "chrome":
+        options = webdriver.ChromeOptions()
+    elif browser == "firefox":
+        options = webdriver.FirefoxOptions()
 
-    # driver = webdriver.Remote(command_executor=selenium_grid_url, options=options)
+    driver = webdriver.Remote(command_executor=selenium_grid_url, options=options)
 
     driver.maximize_window()
     request.cls.driver = driver
@@ -73,11 +74,16 @@ def driver(request):
 
 
 def pytest_configure(config):
-    if not os.path.exists('results'):
-        os.makedirs('results')
+    now = datetime.datetime.now()
 
-    now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    report_file = f"results/report_{now}.html"
+    now_d = now.strftime("%Y-%m-%d")
+    res_dir = f'results/{now_d}'
+
+    if not os.path.exists(res_dir):
+        os.makedirs(res_dir)
+
+    now_dt = now.strftime("%Y-%m-%d_%H-%M-%S")
+    report_file = f"{res_dir}/report_{now_dt}.html"
 
     config.option.htmlpath = report_file
 
@@ -97,12 +103,18 @@ def pytest_runtest_makereport(item):
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
             driver = item.funcargs.get("driver")
-            screenshot_path = f"results/{item.name}.png"
+
+            now = datetime.datetime.now()
+            now_d = now.strftime("%Y-%m-%d")
+            res_dir = f'results/{now_d}'
+            png_file = f"/{item.name}.png"
+
+            screenshot_path = res_dir + png_file
             # file_name = report.nodeid.replace("::", "_") + ".png"
             _capture_screenshot(driver, screenshot_path)
             if screenshot_path:
                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-                       'onclick="window.open(this.src)" align="right"/></div>' % screenshot_path
+                       'onclick="window.open(this.src)" align="right"/></div>' % png_file
                 extra.append(pytest_html.extras.image(screenshot_path))
         report.extra = extra
 
